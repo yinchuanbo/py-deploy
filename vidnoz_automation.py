cd0e15bb15789975f99ffe677b82567fb2dbbcb8
YinHao <1469786443@qq.com>
2025-05-16 11:27:41 +0800

    更新

--- vidnoz_automation.py
+++ vidnoz_automation.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException, ElementNotInteractableException, NoSuchElementException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 import time
 import sys
 import json
